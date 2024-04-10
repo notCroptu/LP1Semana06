331f8036b9ea6cdc174b41373307d8366e11e2a3
notCroptu <martinsmariana1972@gmail.com>
2024-04-10 21:41:57 +0100

    PlayerStats Ex6

---
+++ PlayerStats/Player.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace PlayerStats
+{
+    class Player
+    {
+        private float highScore;
+        public float HighScore
+        {
+            get
+            {
+                return highScore;
+            }
+            set
+            {
+                if (value > highScore) highScore = value;
+            }
+        }
+        private int playedGames;
+        private int wonGames;
+        public string Name { get; }
+        public float WinRate
+        {
+            get
+            {
+                if (playedGames == 0 || wonGames == 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return (float) wonGames / (float) playedGames;
+                }
+            }
+        }
+        public Player(string name)
+        {
+            Name = name;
+            highScore = 0;
+            wonGames = 0;
+            playedGames = 0;
+        }
+        public void PlayGame(bool win)
+        {
+            playedGames++;
+            if (win) wonGames++;
+        }
+    }
+}
